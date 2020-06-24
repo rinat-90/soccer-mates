f5d7c9409b4fff2f396103e48730d0eb71c56083
rinat-90 <ribroff@gmail.com>
2020-06-24 10:54:21 -0500

    firebase added

--- src/main.js
+++ src/main.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBVY5kmGjKIEQ12Tj8-0mbKMMaMPILM_ZM",
+      authDomain: "soccer-mates-1fb7a.firebaseapp.com",
+      databaseURL: "https://soccer-mates-1fb7a.firebaseio.com",
+      projectId: "soccer-mates-1fb7a",
+      storageBucket: "soccer-mates-1fb7a.appspot.com",
+      messagingSenderId: "595703259181",
+      appId: "1:595703259181:web:11509f08ed1f409e70d8a4",
+      measurementId: "G-0JHGX535E0"
+    });
+  }
+}).$mount('#app');
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.green.darken1, // #D32F2F
+        secondary: colors.red.lighten4, // #FFCDD2
+        accent: colors.indigo.base // #3F51B5
+      }
+    }
+  }
 })
